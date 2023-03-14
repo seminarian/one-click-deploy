0f979b393e234c1f3762f6533e43979061d99f6f
Pieter Doms <d0ms_007@hotmail.com>
2023-03-14 19:27:18 +0100

    fix: test pieter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { bootstrap, runMigrations } from '@vendure/core';
-import { config } from './vendure-config';
-import { populateOnFirstRun } from './populate';
+import { bootstrap, runMigrations } from "@vendure/core";
+import { config } from "./vendure-config";
+import { populateOnFirstRun } from "./populate";
 
 populateOnFirstRun(config)
-    .then(() => runMigrations(config))
-    .then(() => bootstrap(config))
-    .catch(err => {
-        console.log(err);
-    });
+  .then(() => runMigrations(config))
+  .then(() => bootstrap(config))
+  .catch((err) => {
+    console.log(err);
+    console.log("oiii");
+  });
